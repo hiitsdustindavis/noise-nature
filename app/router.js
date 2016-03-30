b97b00c89893b543d3ddfc9516f6e8ac71d0e36e
dustin davis and trevor elvey <hiitsdustindavis@gmail.com>
2016-03-30 16:41:10 -0700

    install sass and update app.css to app.scss

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function() {
   this.route('about');
   this.route('contact');
   this.route('post-content', {path: '/post-content/:post_id'});
+  this.route('admin');
 });
 
 export default Router;
