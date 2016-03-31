933f8a4e7cc2d08102ae362e5d8637bf51ef1d94
Dustin Davis and Trevor Elvey <trevor.elvey@gmail.com>
2016-03-31 11:56:09 -0700

    begin update post functionality

--- app/components/post-guts.js
+++ app/components/post-guts.js
@@ -13,6 +13,17 @@ export default Ember.Component.extend({
       if(confirm('Delete Post?')) {
         this.sendAction('destroyPost', post);
       }
+    },
+    updatePost(post) {
+      var params = {
+        title: this.get('title'),
+        natureType: this.get('natureType'),
+        trevorSong: this.get('trevorSong'),
+        dustinSong: this.get('dustinSong'),
+        tileImage: this.get('tileImage'),
+        images: this.get('images')
+      };
+      this.setAction('updatePost', post, params);
     }
   }
 });
